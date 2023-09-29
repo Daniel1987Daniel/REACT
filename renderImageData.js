30ea234a387ecbc432750502c36bbc77f3441446
Daniel Sládeček <83771369+Daniel1987Daniel@users.noreply.github.com>
2023-09-30 01:45:02 +0200

    Update renderImageData.js

--- renderImageData.js
+++ renderImageData.js
@@ -15,46 +15,48 @@ const Croatia = () => {
         setrenderPorecImg(dataPorec)
     }
 
-
     return (
-        <div className='all-images'>
-            <h1>WELCOME TO HOLIDAY APP</h1>
-            {/* <h3>CLICK ON YOUR DESTINATION</h3> */}
-            <ul onClick={setBrac}>
-                <li>BRAČ</li>
-            </ul>
-            <ul onClick={setPorec}>
-                <li>POREČ</li>
-            </ul>
-            <ul>
-            {
-                renderBracImg.map((item) => 
-                    <div className="one-image">
-                        <li key={item.id}>
-                            <img src={item.img} alt="" width={550}/>
-                            <h3>{item.description}</h3>
-                            <h3>{item.price}</h3>
-                        </li>
-                    </div>
-                )
-            }
-            </ul>
-            <ul>
-            {
-                renderPorecImg.map((item) => 
-                    <div className="one-image">
-                        <li key={item.id}>
-                            <img src={item.img} alt="" width={550}/>
-                            <h3>{item.description}</h3>
-                            <h3>{item.price}</h3>
-                        </li>
-                    </div>
-                )
-            }
-            </ul>
-            {/* <img src={renderBracImg} alt="" />
-            <img src={renderPorecImg} alt="" /> */}
-        </div>
+        <div>
+            <div className="header">
+                <h1>WELCOME TO HOLIDAY APP</h1>
+                <ul onClick={setBrac}>
+                    <li>BRAČ</li>
+                </ul>
+                <ul onClick={setPorec}>
+                    <li>POREČ</li>
+                </ul>
+            </div>
+            <div>
+                <div className="all-images">
+                    {
+                        renderBracImg.map((item) => 
+                            <div className="one-image">
+                                <li key={item.id}>
+                                    <img src={item.img} alt=""/>
+                                    <h3>{item.description}</h3>
+                                    <h3>{item.price}</h3>
+                                </li>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+            <div>
+                <div className="all-images">
+                    {
+                        renderPorecImg.map((item) => 
+                            <div className="one-image">
+                                <li key={item.id}>
+                                    <img src={item.img} alt="" />
+                                    <h3>{item.description}</h3>
+                                    <h3 className="price">{item.price}</h3>
+                                </li>
+                            </div>
+                        )
+                    }
+                </div>  
+            </div>
+        </div> 
     )
 }
 
